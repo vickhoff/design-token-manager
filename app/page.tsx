bc1d2de5ec32329430e4bc124505f4336fae70b9
vickhoff <dvickhoff@gmail.com>
2026-08-13 16:18:41 +0200

    Add card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
 import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Home() {
 
@@ -27,8 +36,17 @@ export default function Home() {
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1>Design token manager</h1>
+        <Card>
+          <CardHeader>
+            <CardTitle>Choose your file</CardTitle>
+            <CardDescription>Choose the file containing your tokens</CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <Button>Choose file</Button>
+          </CardFooter>
+        </Card>
       </main>
     </div>
   );
