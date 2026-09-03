d441cc7f5981cd0ee1a41178c9d85dd4c2eb78e9
vickhoff <dvickhoff@gmail.com>
2026-09-03 16:50:42 +0200

    Render and sort tokens by name (#5)

--- app/dashboard/_components/TokenTable.tsx
+++ app/dashboard/_components/TokenTable.tsx
@@ -8,35 +8,72 @@ import {
   TableCell,
   TableCaption,
 } from "@/components/ui/table";
-import { TableInput } from "./TableInput";
 import { type Token } from "../../../lib/tokens/types";
 import { firstLetterUpperCase } from "../../../lib/utils";
+import { Palette, Type, SquareDashed, CircleQuestionMark } from "lucide-react";
 
 interface TokenTableProps {
   type: string;
   tokens: Token[];
 }
 
+function renderIcon(type: string) {
+  switch (type) {
+    case "color":
+      return <Palette className="size-4 hidden md:block" aria-hidden="true" />;
+    case "typography":
+      return <Type className="size-4 hidden md:block" aria-hidden="true" />;
+    case "dimension":
+      return (
+        <SquareDashed className="size-4 hidden md:block" aria-hidden="true" />
+      );
+    default:
+      return (
+        <CircleQuestionMark
+          className="size-4 hidden md:block"
+          aria-hidden="true"
+        />
+      );
+  }
+}
+
 export function TokenTable({ type, tokens }: TokenTableProps) {
   return (
     <section className="bg-(--color-surface-default) border border-(--color-border-default) rounded-(--radius-xl) p-4 w-full">
       <h2>{firstLetterUpperCase(type)}</h2>
-      <Table>
+      <Table className="table-fixed">
         <TableHeader>
           <TableRow>
-            <TableHead>Name</TableHead>
-            <TableHead>Value</TableHead>
+            <TableHead className="w-3/5">Name</TableHead>
+            <TableHead className="w-2/5">Value</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell>
-              <p>color.surface.default</p>
-            </TableCell>
-            <TableCell>
-              <TableInput placeholder="#ffffff" />
-            </TableCell>
-          </TableRow>
+          {tokens.map((token) => (
+            <TableRow className="font-mono" key={token.id}>
+              <TableCell>
+                <span className="flex items-center gap-1.5">
+                  {renderIcon(token.type as string)}
+                  <code>{token.id}</code>
+                </span>
+              </TableCell>
+              <TableCell>
+                <span className="flex items-center gap-1.5">
+                  {token.type === "color" ? (
+                    <div
+                      className="w-4 h-4 rounded-sm border border-(--color-border-default)"
+                      style={{ backgroundColor: token.value as string }}
+                    />
+                  ) : null}
+                  <code>
+                    {typeof token.value === "object"
+                      ? `${token.value.value}${token.value.unit}`
+                      : token.value}
+                  </code>
+                </span>
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </section>
