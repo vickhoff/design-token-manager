ac654f48cc3b6d291b64f0a9a8260e4cc7bf3ec5
vickhoff <dvickhoff@gmail.com>
2026-09-20 19:31:26 +0200

    Success colors for the toast

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -57,6 +57,7 @@ function Toast({ className, ...props }: ToastPrimitive.Root.Props) {
         "data-expanded:data-ending-style:data-[swipe-direction=left]:[transform:translateX(calc(var(--toast-swipe-movement-x)-150%))_translateY(var(--offset-y))]",
         "data-expanded:data-ending-style:data-[swipe-direction=right]:[transform:translateX(calc(var(--toast-swipe-movement-x)+150%))_translateY(var(--offset-y))]",
         "data-expanded:data-ending-style:data-[swipe-direction=up]:[transform:translateY(calc(var(--toast-swipe-movement-y)-150%))]",
+        "data-[type=success]:border-border-success data-[type=success]:bg-surface-success",
         className,
       )}
       {...props}
